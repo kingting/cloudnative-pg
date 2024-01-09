9b9f16fce46b64ea319f57f02153908da6b31947
King Ting <king@ozhut.com>
2024-01-09 11:34:30 +0000

    Added local registry when starting k3d

--- scripts/start-k3d.sh
+++ scripts/start-k3d.sh
@@ -3,11 +3,15 @@
 # Define cluster name
 CLUSTER_NAME="k3d-cluster"
 
+# Create the registry
+k3d registry create localhost --port 5000
+
 # Create a K3d cluster
-k3d cluster create $CLUSTER_NAME
+k3d cluster create $CLUSTER_NAME --registry-use localhost:5000
 
 # Merge K3d kubeconfig to default kubeconfig and switch context
 k3d kubeconfig merge $CLUSTER_NAME --kubeconfig-switch-context
 
 echo "K3d cluster $CLUSTER_NAME started and kubeconfig is set."
-
+echo "To test local registry:"
+echo "curl -X GET http://localhost:5000/v2/"
